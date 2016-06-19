935f62045cb1c4442667b03e272bebf08c3a7367
urzp <urzp002@mail.ru>
2016-06-19 08:51:26 +0300

    Make a structure

---
+++ gallows.rb
@@ -0,0 +1,24 @@
+
+# Gallows
+
+arr_words = File.readlines("5desk.txt")
+
+class Game
+  def initialize ()
+  end
+  
+  def draw_board()
+  end
+  
+  def play
+  end
+  
+  def win_lose?
+  end
+  
+  def save
+  end
+  
+  def load
+  end
+end
