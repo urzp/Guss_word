709aa506b39ceb1401c8f2f6f25b32ec9cd2696b
urzp <urzp002@mail.ru>
2016-06-19 19:00:51 +0300

    check_symbol and win?

--- gallows.rb
+++ gallows.rb
@@ -13,26 +13,41 @@ def load_words(file_name="5desk.txt")
 end
 
 class Game
-  
+  attr_read = :board[:mistakes]
 
   def initialize ()
     @board={}
-    @board[:word] = take_rnd_word($words)
+    @board[:word] = take_rnd_word($words).chomp
 	@board[:turn] = 0
+	@board[:step_paly]=1
 	@board[:mistakes]=[]
 	@board[:opened_symbols]=[]
   end
   
   def draw_board()
-      word = hiden_word(@board[:word], @board[:opened_symbols])
+    
+    puts @board
+    puts "Play step: #{ @board[:step_paly] }"
+    word = hiden_word(@board[:word], @board[:opened_symbols])
 	puts "Word: #{word}"	
-	puts "Misatakes (#{@board[:mistakes].size}): #{ (@board[:mistakes]).join(",") } "
+	puts "Misatakes (#{@board[:mistakes].size}): #{ (@board[:mistakes]).join(", ") } "
   end
   
   def play
+    selection = ""
+    print "Your letter: "
+	while !selection.between?("a", "z") 
+	  selection = gets.chomp.to_s.downcase
+	  if !selection.between?("a", "z") 
+	    puts "Wrong symbol, please puts letter (a..z)" 
+	  end
+	end
+	check_symbol(selection)
+	@board[:step_paly] += 1 
   end
   
-  def win_lose?
+  def win?
+    return true if @board[:word].split(//).all?{ |i| @board[:opened_symbols].include?(i) }
   end
   
   def save
@@ -48,14 +63,26 @@ private
   
   def hiden_word(word, opened_symbols)
     h_word=""
-	0.upto(word.length) do |i|
+	0.upto(word.length - 1) do |i|
 	  opened_symbols.any? { |op_sy| op_sy == word[i] }? h_word += " " + word[i] : h_word += " _"
 	end
 	return h_word
+  end
+  
+  def check_symbol(selection)
+    @board[:word].include?(selection) ? @board[:opened_symbols] << selection : @board[:mistakes] << selection 
   end
 end
 
 
 load_words()
 game = Game.new
-game.draw_board
+while 
+  game.draw_board
+  game.play
+  if game.win?
+    game.draw_board
+    puts "You win" 
+	break
+  end
+end
